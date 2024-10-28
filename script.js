047a3030d4c216902d1228656672167d2c8f8479
jose <josesaa0324@gmail.com>
2024-10-28 15:27:16 -0500

    formula y contenido en js

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ let peso = parseInt(prompt
 let altura = parseInt(prompt
         ("altura"))
 
+        let imc = peso / (altura * altura);
+
+
+        let clasificacion;
         
+        if (imc < 18.5) {
+            clasificacion = "peso bajo";
+        } else if (imc >= 18.5 && imc < 25) {
+            clasificacion = "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            clasificacion = "Sobrepeso";
+        } else if (imc >= 30 && imc < 35) {
+            clasificacion = "Obesidad clase 1";
+        } else if (imc >= 35 && imc < 40) {
+            clasificacion = "obesidad clase 2"
+        } else {
+            clasificacion = "obesidad clase 3"
+        }        
